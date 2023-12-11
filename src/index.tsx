d6f27e5fab5ca3b40b53256b9ef471b99c5aa73c
devin han <devinhworks@gmail.com>
2023-12-11 01:43:19 -0800

    Remove redundant files

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { store } from "./store/store.ts";
 import { Provider } from "react-redux";
 
